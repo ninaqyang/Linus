5f4932d913d1d8af27361907d9f46045a5e2553b
Nina Yang <ninaqy13@gmail.com>
2017-05-13 23:38:07 -0400

    Basically rewrote everything to use speech kit cause I gave up on Audio kit

--- Linus/OrderViewController.swift
+++ Linus/OrderViewController.swift
@@ -11,33 +11,21 @@ import ExpandingCollection
 import ConversationV1
 import AudioKit
 import SpeechToTextV1
+import Speech
 
-class OrderViewController: UIViewController {
-    
-    var recorder: AKNodeRecorder!
-    var player: AKAudioPlayer!
-    var delay: AKDelay!
-    
-    var recordingState = RecordingState.readyToRecord
+class OrderViewController: UIViewController, SFSpeechRecognizerDelegate {
     
     var speechToText: SpeechToText!
     var speechToTextSession: SpeechToTextSession!
     
-    var isStreaming = false
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var microphoneButton: UIButton!
     
-    @IBOutlet fileprivate var inputPlot: AKNodeOutputPlot!
-    @IBOutlet fileprivate weak var infoLabel: UILabel!
-    @IBOutlet var recordLongPress: UILongPressGestureRecognizer!
-    @IBOutlet fileprivate weak var recordButton: UIButton!
-//    @IBOutlet fileprivate weak var textView: UITextView!
-    @IBOutlet fileprivate weak var speechLabel: UILabel!
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-US"))
 
-    enum RecordingState {
-        case readyToRecord
-        case recording
-        case readyToPlay
-        case playing
-    }
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,148 +40,128 @@ class OrderViewController: UIViewController {
             password: Credentials.SpeechToTextPassword
         )
         
-        // Clean tempFiles !
-        AKAudioFile.cleanTempDirectory()
+        microphoneButton.isEnabled = false  //2
         
-        // Session settings
-        AKSettings.bufferLength = .medium
+        speechRecognizer?.delegate = self  //3
+        
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in  //4
+            
+            var isButtonEnabled = false
+            
+            switch authStatus {  //5
+            case .authorized:
+                isButtonEnabled = true
+                
+            case .denied:
+                isButtonEnabled = false
+                print("User denied access to speech recognition")
+                
+            case .restricted:
+                isButtonEnabled = false
+                print("Speech recognition restricted on this device")
+                
+            case .notDetermined:
+                isButtonEnabled = false
+                print("Speech recognition not yet authorized")
+            }
+            
+            OperationQueue.main.addOperation() {
+                self.microphoneButton.isEnabled = isButtonEnabled
+            }
+        }
+    }
+    
+    fileprivate func startRecording() {
+        
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryRecord)
+            try audioSession.setMode(AVAudioSessionModeMeasurement)
+            try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+        } catch {
+            print("audioSession properties weren't set because of an error.")
+        }
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        guard let inputNode = audioEngine.inputNode else {
+            fatalError("Audio engine has no input node")
+        }
+        
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+        }
+        
+        recognitionRequest.shouldReportPartialResults = true
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+            
+            var isFinal = false
+            
+            if result != nil {
+                
+                self.textView.text = result?.bestTranscription.formattedString
+                isFinal = (result?.isFinal)!
+            }
+            
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.microphoneButton.isEnabled = true
+            }
+        })
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
         
         do {
-            try AKSettings.setSession(category: .playAndRecord, with: .allowBluetoothA2DP)
+            try audioEngine.start()
         } catch {
-            AKLog("Could not set session category.")
+            print("audioEngine couldn't start because of an error.")
         }
-
-        AKSettings.defaultToSpeaker = true
         
-        // Patching
-        let mic = AKMicrophone()
-        inputPlot.node = mic
-        let micMixer = AKMixer(mic)
+        textView.text = "Say something, I'm listening!"
         
-        // Will set the level of microphone monitoring
-        recorder = try? AKNodeRecorder(node: micMixer)
-        if let file = recorder.audioFile {
-            player = try? AKAudioPlayer(file: file)
-        }
-        player.looping = true
-        player.completionHandler = playingEnded
-        
-        AudioKit.start()
-        
-        setupUIForRecording()
     }
-    
-    // CallBack triggered when playing has ended
-    // Must be seipatched on the main queue as completionHandler
-    // will be triggered by a background thread
-    fileprivate func playingEnded() {
-        DispatchQueue.main.async {
-            self.setupUIForPlaying()
-        }
-    }
-    
-    fileprivate func setupUIForRecording() {
-        recordingState = .readyToRecord
-//        infoLabel.text = "Ready to record"
-        recordButton.setTitle("Record", for: .normal)
-    }
-    
-    fileprivate func setupUIForPlaying() {
-        //TODO: FIX THIS
-        let recordedDuration = player != nil ? player.audioFile.duration  : 0
-//        infoLabel.text = "Recorded: \(String(format: "%0.1f", recordedDuration)) seconds"
-        recordButton.setTitle("Play", for: .normal)
-        recordingState = .readyToPlay
-    }
-    
     
     // MARK: Actions
-
-    @IBAction func recordButtonPressed(_ sender: Any) {
-        if self.recordLongPress.state == .began {
-            if self.recordingState == .readyToRecord {
-                // infoLabel.text = "Recording"
-                recordButton.setTitle("Stop", for: .normal)
-                recordingState = .recording
-                
-                do {
-                    try recorder.record()
-                } catch {
-                    print("Errored recording.")
-                }
-                
-                recordButton.setTitle("Stop Microphone", for: .normal)
-            }
-            self.beginLongPress()
-        } else if self.recordLongPress.state == .ended {
-            self.endLongPress()
-        } else if self.recordLongPress.state == .failed {
-            //handle error here basically do nothing
-            return
+    
+    @IBAction func microphoneTapped(_ sender: AnyObject) {
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            microphoneButton.isEnabled = false
+            microphoneButton.setTitle("Start Recording", for: .normal)
+        } else {
+            startRecording()
+            microphoneButton.setTitle("Stop Recording", for: .normal)
         }
     }
     
-    fileprivate func beginLongPress() {
-        // Microphone monitoring is muted
-        recordButton.setTitle("Start Microphone", for: .normal)
-        
-        do {
-            try player.reloadFile()
-        } catch { print("Errored reloading.") }
-        
-        let recordedDuration = player != nil ? player.audioFile.duration  : 0
-        if recordedDuration > 0.0 {
-            recorder.stop()
-            player.audioFile.exportAsynchronously(name: "TempTestFile.m4a",
-                                                  baseDir: .documents,
-                                                  exportFormat: .m4a) {_, exportError in
-                                                    if let error = exportError {
-                                                        print("Export Failed \(error)")
-                                                    } else {
-                                                        print("Export succeeded")
-                                                    }
-            }
-            setupUIForPlaying()
-        }
+    
+    // MARK: SFSpeechRecognizerDelegate
 
-        player.play()
-        //            infoLabel.text = "Playing..."
-        recordButton.setTitle("Stop", for: .normal)
-        recordingState = .playing
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            microphoneButton.isEnabled = true
+        } else {
+            microphoneButton.isEnabled = false
+        }
     }
     
-    fileprivate func endLongPress() {
-        player.stop()
-        setupUIForPlaying()
-    }
-    
-    fileprivate func startStreamingMicrophone() {
-        // define callbacks
-        speechToTextSession.onConnect = { print("connected") }
-        speechToTextSession.onDisconnect = { print("disconnected") }
-        speechToTextSession.onError = { error in print(error) }
-        speechToTextSession.onPowerData = { decibels in print(decibels) }
-        speechToTextSession.onMicrophoneData = { data in print("received data") }
-        speechToTextSession.onResults = { results in self.speechLabel.text = results.bestTranscript }
-        
-        // define recognition settings
-        var settings = RecognitionSettings(contentType: .opus)
-        settings.continuous = true
-        settings.interimResults = true
-        
-        // start recognizing microphone audio
-        speechToTextSession.connect()
-        speechToTextSession.startRequest(settings: settings)
-        speechToTextSession.startMicrophone()
-    }
-    
-    fileprivate func stopStreamingMicrophone() {
-        // stop recognizing microphone audio
-        speechToTextSession.stopMicrophone()
-        speechToTextSession.stopRequest()
-        speechToTextSession.disconnect()
-    }
     
     class func instanceFromNib() -> OrderViewController {
         return UINib(nibName: "OrderViewController", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as! OrderViewController
